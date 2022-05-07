d31bee13380ce42055e47c71c381caf64ec2f7d5
qowngus33 <83813866+qowngus33@users.noreply.github.com>
2022-05-07 23:12:57 +0900

    Add timeAttackmode timer

--- src/main/java/game/FightMenu.java
+++ src/main/java/game/FightMenu.java
@@ -23,9 +23,11 @@ public class FightMenu extends JFrame {
 	private static int initInterval;
 	private JLabel [] scoreLabel;
 	private JLabel [] lineLabel;
+	private JLabel [] timeLabel;
 
 	private Timer timer;
-	private final long startTime;
+	private Timer timeAttackTimer;
+	private long startTime;
 	private final long exitTime = 60000;
 	private final boolean isTimeAttackMode;
 	private boolean isGameEnded;
@@ -48,18 +50,23 @@ public class FightMenu extends JFrame {
 		// lower panel
 		JButton settingButton = new JButton("Settings");
 		JButton startMenuBtn = new JButton("Start Menu");
-		JButton scoreBoardBtn = new JButton("EXIT");
+		JButton exitBtn = new JButton("EXIT");
 		JButton restartBtn = new JButton("restart");
 		settingButton.addActionListener(e -> btnSettingActionPerformed());
 		startMenuBtn.addActionListener(e -> btnStartMenuActionPerformed());
-		scoreBoardBtn.addActionListener(e -> btnScoreBoardActionPerformed());
+		exitBtn.addActionListener(e -> btnExitActionPerformed());
 		restartBtn.addActionListener(e -> btnRestartActionPerformed());
 		JPanel lowerPanel = new JPanel(new GridLayout(0, 4));
 		lowerPanel.add(settingButton);
 		lowerPanel.add(startMenuBtn);
-		lowerPanel.add(scoreBoardBtn);
+		lowerPanel.add(exitBtn);
 		lowerPanel.add(restartBtn);
 
+		//Time Attack Mode
+		if(isTimeAttackMode){
+			startTime = currentTimeMillis();
+			setTimeAttackMode();
+		}
 		JPanel upperPanel = new JPanel(new GridLayout(0, 2));
 		gameBoard = new GameBoard[2];
 		gamePane = new GamePane[2];
@@ -83,7 +90,6 @@ public class FightMenu extends JFrame {
 				+"\nPLAYER2:"+settingItem.getP2LeftKey()+" "+ settingItem.getP2RightKey()+" "+settingItem.getP2DownKey()+" "
 				+settingItem.getP2RotateKey()+" "+settingItem.getP2DropKey();
 		JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), message);
-		startTime = currentTimeMillis();
 		setTimer();
 		setFocusable(true);
 		requestFocus();
@@ -125,6 +131,8 @@ public class FightMenu extends JFrame {
 		// left panel
 		JPanel rightPanel = new JPanel();
 		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+		if(SettingItem.isTimeAttackMode)
+			rightPanel.add(timeLabel[i]);
 		rightPanel.add(gameBoard[i].nextBlockPane);
 		rightPanel.add(scoreLabel[i]);
 		rightPanel.add(lineLabel[i]);
@@ -161,35 +169,35 @@ public class FightMenu extends JFrame {
 		am.put("right1", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning())
+				if (timer.isRunning() && !isGameEnded)
 					gameBoard[0].moveRight();
 			}
 		});
 		am.put("left1", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning())
+				if (timer.isRunning()&& !isGameEnded)
 					gameBoard[0].moveLeft();
 			}
 		});
 		am.put("up1", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning())
+				if (timer.isRunning()&& !isGameEnded)
 					gameBoard[0].rotateBlock();
 			}
 		});
 		am.put("down1", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning())
+				if (timer.isRunning()&& !isGameEnded)
 					moveDown(0);
 			}
 		});
 		am.put("space1", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning()) {
+				if (timer.isRunning()&& !isGameEnded) {
 					gameBoard[0].dropBlock();
 					if(newBlock(0)){
 						gameBoard[0].placeBlock();
@@ -205,35 +213,35 @@ public class FightMenu extends JFrame {
 		am.put("right2", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning())
+				if (timer.isRunning() && !isGameEnded)
 					gameBoard[1].moveRight();
 			}
 		});
 		am.put("left2", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning())
+				if (timer.isRunning() && !isGameEnded)
 					gameBoard[1].moveLeft();
 			}
 		});
 		am.put("up2", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning())
+				if (timer.isRunning() && !isGameEnded)
 					gameBoard[1].rotateBlock();
 			}
 		});
 		am.put("down2", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning())
+				if (timer.isRunning() && !isGameEnded)
 					moveDown(1);
 			}
 		});
 		am.put("space2", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (timer.isRunning()) {
+				if (timer.isRunning() && !isGameEnded)  {
 					gameBoard[1].dropBlock();
 					if(newBlock(1)){
 						gameBoard[1].placeBlock();
@@ -263,6 +271,22 @@ public class FightMenu extends JFrame {
 		timer.start();
 	}
 
+	protected void setTimeAttackMode(){
+		timeLabel = new JLabel[2];
+		timeLabel[0] = new JLabel((exitTime-(currentTimeMillis()-startTime))+"");
+		timeLabel[1] = new JLabel((exitTime-(currentTimeMillis()-startTime))+"");
+		timeAttackTimer= new Timer(10, e -> {
+			if(isTimeAttackMode && currentTimeMillis()-startTime>exitTime){
+				gameOver();
+			} else {
+				timeLabel[0].setText(((exitTime-(currentTimeMillis()-startTime))/1000)+"");
+				timeLabel[1].setText(((exitTime-(currentTimeMillis()-startTime))/1000)+"");
+			}
+		});
+		timeAttackTimer.start();
+	}
+
+
 	protected void updateScore() {
 		for(int i=0;i<2;i++){
 			scoreLabel[i].setText(String.format("%5s", gameBoard[i].getScore() + "") + " ");
@@ -281,11 +305,11 @@ public class FightMenu extends JFrame {
 	}
 
 	public void pause() {
-		if (!timer.isRunning()) {
+		if (!timer.isRunning() && !isGameEnded) {
 			timer.start();
 			gameBoard[0].drawBoard();
 			gameBoard[1].drawBoard();
-		} else {
+		} else if(timer.isRunning() && !isGameEnded) {
 			timer.stop();
 			gameBoard[0].setGameBoardText("PAUSE");
 			gameBoard[1].setGameBoardText("PAUSE");
@@ -309,18 +333,13 @@ public class FightMenu extends JFrame {
 	protected boolean newBlock(int i) {
 		gameBoard[i].placeBlock(); // 밑으로 내려가지 않게 고정
 		System.out.println(currentTimeMillis()-startTime);
-		if (gameBoard[i].isGameEnded() || (isTimeAttackMode && currentTimeMillis()-startTime>exitTime)) {
-			timer.stop();
-			isGameEnded = true;
+		if (gameBoard[i].isGameEnded()) {
 			gameOver();
 			return false;
 		}
-		int temp = (i == 0) ? 1:0;
-		gameBoard[i].gamePane.addLines(gameBoard[temp].erasedLine);
-		gameBoard[temp].resetErasedLine();
-		gamePane[i].setLines(gameBoard[temp].getErasedLine());
-
+		setErasedLine(i);
 		boolean isErased = gameBoard[i].eraseLine();
+
 		gameBoard[i].curr = gameBoard[i].nextBlock;
 		if(SettingItem.isItemMode && (gameBoard[i].lineNum/gameBoard[i].count >= gameBoard[i].lineChange)) {
 			gameBoard[i].nextBlock = gameBoard[i].getRandomBlock.getItemBlock(gameBoard[i].modeName);
@@ -333,7 +352,18 @@ public class FightMenu extends JFrame {
 		gameBoard[i].nextBlockPane.drawNextBlockBoard(gameBoard[i].getNextBlock());
 		return isErased;
 	}
+
+	protected void setErasedLine(int i){
+		int temp = (i == 0) ? 1:0;
+		gameBoard[i].gamePane.addLines(gameBoard[temp].erasedLine);
+		gameBoard[temp].resetErasedLine();
+		gamePane[i].setLines(gameBoard[temp].getErasedLine());
+	}
+
 	private void gameOver() {
+		timer.stop();
+		timeAttackTimer.stop();
+		isGameEnded = true;
 		String text = "draw";
 		if(gameBoard[0].isGameEnded() && !gameBoard[1].isGameEnded()) {
 			gameBoard[0].setGameBoardText("LOSE");
@@ -343,9 +373,6 @@ public class FightMenu extends JFrame {
 			gameBoard[0].setGameBoardText("WIN!");
 			gameBoard[1].setGameBoardText("LOSE");
 			text = "player1 win";
-		} else if(gameBoard[0].isGameEnded() && gameBoard[1].isGameEnded()){
-			gameBoard[0].setGameBoardText("DRAW!");
-			gameBoard[1].setGameBoardText("DRAW!");
 		} else{
 			if(gameBoard[0].score>gameBoard[1].score){
 				gameBoard[0].setGameBoardText("WIN!");
@@ -355,7 +382,7 @@ public class FightMenu extends JFrame {
 				gameBoard[0].setGameBoardText("LOSE");
 				gameBoard[1].setGameBoardText("WIN!");
 				text = "player2 win";
-			} else{
+			} else if(gameBoard[0].score==gameBoard[1].score){
 				gameBoard[0].setGameBoardText("DRAW!");
 				gameBoard[1].setGameBoardText("DRAW!");
 			}
@@ -363,26 +390,30 @@ public class FightMenu extends JFrame {
 		JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), text);
 	}
 
-	private void btnScoreBoardActionPerformed() {
+	private void btnExitActionPerformed() {
 		timer.stop();
+		timeAttackTimer.stop();
 		dispose();
 		System.exit(0);
 	}
 
 	private void btnSettingActionPerformed() {
 		timer.stop();
+		timeAttackTimer.stop();
 		dispose();
 		Tetris.showSettingMenu();
 	}
 
 	private void btnStartMenuActionPerformed() {
 		timer.stop();
+		timeAttackTimer.stop();
 		dispose();
 		Tetris.showStartMenu();
 	}
 
 	private void btnRestartActionPerformed() {
 		timer.stop();
+		timeAttackTimer.stop();
 		dispose();
 		try {
 			Tetris.start(false);
