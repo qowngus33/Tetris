e22d78f17670baa45de775c09747a729d91e783e
Weble21 <89642807+Weble21@users.noreply.github.com>
2022-05-26 16:16:58 +0900

    fight mode keyListener add

---
+++ src/test/java/component/StartMenuTest.java
@@ -0,0 +1,40 @@
+package component;
+
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import scoreboard.ScoreBoardFile;
+
+public class StartMenuTest {
+    StartMenu startMenu = new StartMenu();
+
+    @Test
+    public void keyTest() {
+        Robot robot;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+        for (int i = 0; i < 100; i++) {
+            assertTimeout(ofMillis(600), () -> {
+                robot.keyPress(KeyEvent.VK_LEFT);
+                robot.keyPress(KeyEvent.VK_RIGHT);
+                robot.keyPress(KeyEvent.VK_DOWN);
+                robot.keyPress(KeyEvent.VK_UP);
+                robot.keyPress(KeyEvent.VK_D);
+                robot.keyPress(KeyEvent.VK_ENTER);
+                robot.keyPress(KeyEvent.VK_SPACE);
+            });
+        }
+        assertTimeout(ofMillis(300), () -> {
+            robot.keyPress(KeyEvent.VK_LEFT);
+        });
+    }
+}
